4f150590bed2bebf9f830d44289edcd719e86af3
Sankar <sankarranga2309@gmail.com>
2023-11-02 18:15:19 +0530

    inital commit with find color

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchView from './searchColorCardByWord';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchView />
+    </div>
+  );
+}
+
+export default App;
